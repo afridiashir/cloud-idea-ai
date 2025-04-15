2c913431f823675398bf28fc2427e4d1fc26bb63
asherafridi <ashirafridi.work@gmail.com>
2025-04-15 12:49:23 +0500

    login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -80,12 +80,12 @@ export default function Login() {
         <div className="px-8 pt-8">
           <Image src="/logo.png" width={150} height={120} alt="Logo" />
         </div>
-        <div className="md:hidden font-heading text-3xl font-bold flex flex-col-reverse lg:flex-row gap-8 items-center justify-center mb-5">
+        <div className="md:hidden font-heading text-2xl font-bold flex flex-row gap-8 items-center justify-center mb-5">
           <h1 className="text-center">
             Bring <span className="text-primary">Ideas</span> to{" "}
             <span className="text-primary">Life!</span>
           </h1>
-          <Image src={Graphic} alt="graphic" className="w-16" />
+          <Image src={Graphic} alt="graphic" className="w-12" />
         </div>
         <div className="py-[50px] px-[20px] lg:py-[50px] lg:px-[70px]">
           <h2 className="text-center text-3xl font-bold mb-8">Login</h2>
